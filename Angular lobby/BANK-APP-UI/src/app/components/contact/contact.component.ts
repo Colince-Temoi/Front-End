cbbd93e9d3b43259024f60de69fcb4c7c4ccac21
TMI_7 <tmi2023@gamil.com>
2024-10-30 09:18:52 +0300

    Method level security @Pre/PostFilter hand-on, for details check notes in book

--- Angular lobby/BANK-APP-UI/src/app/components/contact/contact.component.ts
+++ Angular lobby/BANK-APP-UI/src/app/components/contact/contact.component.ts
@@ -12,6 +12,7 @@ import { DashboardService } from 'src/app/services/dashboard/dashboard.service';
 })
 export class ContactComponent implements OnInit {
   model = new Contact();
+  contacts = new Array();
 
   constructor(private dashboardService: DashboardService) {
 
@@ -21,10 +22,20 @@ export class ContactComponent implements OnInit {
       
   }
 
+  /** As output from this behavior, we are receiving an Arrray of Contact objects
+   * Next; we are fetching the very first element from this contacts array  and assigning it to this model object
+   * With the forEach loop we are iterating over each and evry element available inside the contacts array and post that we are going to assign that contact element to the model object
+   * And we are binding the model object to 'this' this class and that why we have to use 'bind(this)'
+   * @param contactForm 
+   */
   saveMessage(contactForm: NgForm) {
     this.dashboardService.saveMessage(this.model).subscribe(
       responseData => {
-        this.model = <any> responseData.body;
+        this.contacts = <any> responseData.body;
+        this.contacts.forEach(function (this: ContactComponent, contact: Contact) {
+          this.model = contact;
+        }.bind(this));
+        // this.model = <any> responseData.body;
         contactForm.resetForm();
       });
 
